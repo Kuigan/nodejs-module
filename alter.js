567cbba18caeba78084f0eb25e98e36c337e6332
Kuigan <info@kuigan.de>
2024-04-08 17:20:37 +0200

    Hausaufgabe vom 08.04.2024. Kleine Code-Korrekturen.

--- alter.js
+++ alter.js
@@ -5,5 +5,5 @@ import chalk from 'chalk';
 export function summe(x, y) {
   const z = x + y;
   // Verwendung von Chalk, um die Konsolenausgabe bunt zu machen
-  console.log(chalk.blue(`Die Summe von Zahlen `) + chalk.red.bold(`${x} `) + chalk.blue(`und `) + chalk.red.bold(`${y} `) + chalk.blue(`ist `) + chalk.red.bold(`${z} `) + chalk.blue(`.`));
+  console.log(chalk.blue(`Die Summe von Zahlen `) + chalk.red.bold(`${x} `) + chalk.blue(`und `) + chalk.red.bold(`${y}`) + chalk.blue(`ist `) + chalk.red.bold(`${z} `) + chalk.blue(`.`));
 }
--- index.js
+++ index.js
@@ -4,7 +4,3 @@ import { summe } from "./alter.js";
 summe(5, 7);
 summe(10, 15);
 summe(20, 30);
-
-
-import chalk from 'chalk';
-// console.log(chalk.blue('Hello world!'));
