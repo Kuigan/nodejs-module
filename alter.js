93443161b34963617f53dc95035d3a0dbc8c5e82
Kuigan <info@kuigan.de>
2024-04-08 17:14:51 +0200

    Hausaufgabe vom 08.04.2024.

---
+++ alter.js
@@ -0,0 +1,9 @@
+// Importieren des Chalk-Moduls
+import chalk from 'chalk';
+
+// Definition und Export der Funktion summe
+export function summe(x, y) {
+  const z = x + y;
+  // Verwendung von Chalk, um die Konsolenausgabe bunt zu machen
+  console.log(chalk.blue(`Die Summe von Zahlen `) + chalk.red.bold(`${x} `) + chalk.blue(`und `) + chalk.red.bold(`${y} `) + chalk.blue(`ist `) + chalk.red.bold(`${z} `) + chalk.blue(`.`));
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+import { summe } from "./alter.js";
+
+// Funktion 3 Mal aufrufen
+summe(5, 7);
+summe(10, 15);
+summe(20, 30);
+
+
+import chalk from 'chalk';
+// console.log(chalk.blue('Hello world!'));
